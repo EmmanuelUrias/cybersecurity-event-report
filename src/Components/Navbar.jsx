7b4f07b2202c620ffd56c7a91ab9cb2f3c743468
EmmanuelU <emmanuelurias60@icloud.com>
2023-03-05 16:29:23 -0800

    last changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,11 +7,11 @@ function NavBar() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">CyberSecurity News</Navbar.Brand>
+          <Navbar.Brand href="/home">CyberSecurity News</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Trending News</Nav.Link>
-            <Nav.Link href="#pricing">More News</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/trending-news">Trending News</Nav.Link>
+            <Nav.Link href="/more-news">More News</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
